102818ee4948398b5756105b992327e52f6f95d1
PapaZrix <zrilic313@gmail.com>
2022-12-01 13:22:18 +0100

    Apply media queries and fix some styling issues

--- src/contact.js
+++ src/contact.js
@@ -6,18 +6,23 @@ export default function loadContactPage() {
     contact.classList.add("contact");
     main.appendChild(contact);
     return contact.innerHTML = `
-    <div class="phone">
-        <span class="material-symbols-outlined">
-        call
-        </span>
-        <p>123 456 789</p>
-        </div>
-    <div class="address">
-        <span class="material-symbols-outlined">
-        home
-        </span>
-        <p class="address">1111 South Broadway, Los Angeles, USA</p>
-    </div>
+    <p class="phone"> <span class="material-symbols-outlined">call</span> 123 456 789</p> 
+    <p class="address"> <span class="material-symbols-outlined">
+    home
+    </span> 1111 South Broadway, Los Angeles, USA </p>
     <img src="pics/map.png" alt="">
     `
 }
+
+/* <div class="phone">
+<span class="material-symbols-outlined">
+call
+</span>
+<p>123 456 789</p>
+</div>
+<div class="address">
+<span class="material-symbols-outlined">
+home
+</span>
+<p class="address">1111 South Broadway, Los Angeles, USA</p>
+</div> */
